070c031adfd79bfb6c486167dc7bc0ef3deeda29
Julia Miskey <juliamiskey@gmail.com>
2018-05-08 16:49:26 -0400

    Adding files:

---
+++ script.js
@@ -0,0 +1,26 @@
+// URL: https://api.themoviedb.org/3/movie/550?api_key=4596c01cc129c0cbf8d2d1ac4cf4b6fe
+// KEY: 4596c01cc129c0cbf8d2d1ac4cf4b6fe
+// USERNAME: LiYingCodes
+// PW:juliying
+// DEVLINK: https://developers.themoviedb.org/3/discover/tv-discover
+
+const app = { };
+
+app.init = function () {
+    console.log('');
+
+};
+
+$(function () {
+    app.init();
+});
+
+    "genres": {
+        Comedy: 35,
+        Drama: 18,
+        Family: 10751,
+        Romance: 10749,
+        Science Fiction: 878,
+        Thriller : 53
+    }
+
