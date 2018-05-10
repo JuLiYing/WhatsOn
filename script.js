114f9091d8f322b9b89d95deeeb2b0b74672fe4b
Li Ying Lim <liying1990@gmail.com>
2018-05-10 17:55:00 -0400

    added styles, fixed dropdown, made progress with value retrieval

--- script.js
+++ script.js
@@ -36,7 +36,6 @@ app.getTV = function (selectedGenre, selectedYear){
         const tvObjects = res.results;
       console.log(tvObjects);
         app.displayTV(tvObjects);
-
     });
 };
 
@@ -53,59 +52,37 @@ app.displayTV = function (tv) {
         $('#tv-results').append($TVContainer);
     })
 };
-// Create event listeners that retrieve user input for both parameters
+// Create event listeners that retrieve user input for both parameters: genre and year
 app.events = function () {
-
-
-// Create event listener for $('.genreForm').on('submit', function () {}); 
-// Collect user input for genre and store in variable called selectedGenre
-    let selectedGenre
-$(".genreForm").on("submit", function(e) {
-    e.preventDefault();
-    selectedGenre = $('input[name=genre]:checked').val();
-    console.log(selectedGenre);
-});
-// Create event listener for $('.yearForm').on('submit', function () {});
-// Collect user input for year and store in variable called selectedYear
-
-$(".decadeForm").on("change", function() {
-    console.log($(this).val());
+    $('.form').on('submit', function (e) {
+        // Prevent page refresh on submit
+        e.preventDefault();
+        // Collect user input for genre and store in variable called selectedGenre
+        const selectedGenre = $('input[name=genre]:checked').val();   
+        // console.log(selectedGenre);
+        // Collect user input for year and store in variable called selectedYear    
+        const selectedYear = $('.yearForm:not(.hidden)').val();
+        // console.log(selectedYear);       
     
-    if ($(this).val() === "80") {
-        $(".80").toggleClass("hidden");
-    }
-    if ($(this).val() === "90") {
-        $(".90").toggleClass("hidden");
-        // $(".80").hide(".80");
-    } 
-    if ($(this).val() === "2000") {
-        $(".2000").toggleClass("hidden");
-        // $(".90").remove(".90");
-    } 
-    if ($(this).val() === "2010") {
-        $(".2010").toggleClass("hidden");
-        // $(".2000").remove(".2000");
-    }
-});
-    let selectedYear;
-$(".yearForm").on("change", function() {
-    // const selectedYear = $(".yearForm option:selected").val();
-     selectedYear = $(this).val();
-    // console.log(selectedYear);
-    app.getTV(selectedGenre, selectedYear);
-});
+        userSelection = {
+            genre: selectedGenre,
+            year: selectedYear
+        }
+        
+        console.log(userSelection);     
+    });
 
+// Toggle the class of hidden on the year dropdown corresponding to the decade dropdown
+$('#decadeForm').change(function () {
+        let val = $('#decadeForm').val();
+        $(".yearForm").addClass("hidden");
+        if (val) {
+            $('#year' + val).toggleClass("hidden");
+        }
+    });
 }
 
-
-
-// Toggle the class of hidden on the year dropdown corresponding to the decade dropdown 
-
-
-
-
 // STRETCH GOALS PORTION - GETTING SIMILAR TITLES (get similar TV shows method url: https://developers.themoviedb.org/3/tv/get-similar-tv-shows)
-
 app.getSimilar = function(tv_id){
     return $.ajax({
         url: `${baseURL}/tv/${tv_id}/similar`,
